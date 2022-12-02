c56d716079f8e8f9b5a62c11113685452761b9cc
FacundoMartinez14 <facundo.eet2@gmail.com>
2022-12-02 17:46:42 -0300

    cors

--- api/src/app.js
+++ api/src/app.js
@@ -14,10 +14,6 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-server.use(cors({
-  origin: ["http://localhost:3000/", 'https://countries-app-mmov.vercel.app/'],
-  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
-}));
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
